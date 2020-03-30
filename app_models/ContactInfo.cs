c5ef9bd2341751d848cf6a67c1882cc4707627ba
nbourre <nbourrereg@hotmail.com>
2020-03-30 13:42:08 -0400

    Add project files.

---
+++ app_models/ContactInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace app_models
+{
+    public class ContactInfo
+    {
+        public string ContactType { get; set; }
+        public string Contact { get; set; }
+
+        public string Info => $"{ContactType} : {Contact}";
+    }
+}
---
+++ app_models/IDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app_models
+{
+    interface IDataService<T>
+    {
+        IEnumerable<T> GetAll();
+    }
+}
